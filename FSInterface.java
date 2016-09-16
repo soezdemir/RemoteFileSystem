e40eb313b5fb5cadedf290a61f9fd7d1c62d709e
soezdemir <sadri.oezdemir@googlemail.com>
2016-09-16 11:02:49 +0200

    Add files via upload

---
+++ FSInterface.java
@@ -0,0 +1,29 @@
+//package src.rmifs;
+package rmifs;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * RMI-Klasse zum Darstellen eines Dateisystems
+ * @author Marco Palumbo
+ * @version 1.01
+ */
+
+
+public interface FSInterface extends Remote
+{
+	public String browseDirs(String dir) throws RemoteException;
+	public String browseFiles(String dir) throws RemoteException;
+	public String search(String file, String startDir) throws RemoteException;
+	public boolean createFile(String file) throws RemoteException;
+	public boolean createDir(String dir) throws RemoteException;
+	public boolean delete(String file) throws RemoteException;
+	public boolean rename(String oldName, String newName) throws RemoteException;
+	public String getOSName()throws RemoteException;
+	public String getHostName() throws RemoteException;
+	public String getHostAddress() throws RemoteException;
+	public void sendClientAddress(String clientAddress) throws RemoteException;
+	//public String sendClientName(String clientName) throws RemoteException; //ToDo
+	//public String sendClientOS(String clientOS) throws RemoteException; //ToDo
+}
---
+++ VerwalterInterface.java
@@ -0,0 +1,31 @@
+//package src.rmifs;
+package rmifs;
+
+/**
+ * Interface für die VerwalterServer
+ * @author cpatzek & soezdemir
+ * @version 1.03
+ * @date 2016-09-14
+ */
+
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+interface VerwalterInterface extends Remote{
+    public String getServerList() throws RemoteException;
+    public String browseDirs(String dir) throws RemoteException;
+    public String browseFiles(String dir) throws RemoteException;
+    public String search(String file, String startDir) throws RemoteException;
+    public boolean createFile(String file) throws RemoteException;
+    public boolean createDir(String dir) throws RemoteException;
+    public boolean delete(String file) throws RemoteException;
+    public boolean rename(String oldName, String newName) throws RemoteException;
+    public String getOSName()throws RemoteException;
+    public String getHostName() throws RemoteException;
+    public String getHostAddress() throws RemoteException, NotBoundException;
+    public void sendClientAddress(String clientAddress) throws RemoteException;
+    //public String sendClientName(String clientName) throws RemoteException;//ToDo
+    //public String sendClientOS(String clientOS) throws RemoteException; //ToDo
+
+}
